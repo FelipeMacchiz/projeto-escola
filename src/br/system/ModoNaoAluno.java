2628ab7f4e9a5b57e5f543a350940e9b1b060e41
GuxtaGol <74304585+GuxtaGol@users.noreply.github.com>
2020-11-11 11:29:35 -0300

    foi adicionado as informaçoes dos alunos

--- src/br/system/ModoNaoAluno.java
+++ src/br/system/ModoNaoAluno.java
@@ -6,6 +6,18 @@ public class ModoNaoAluno {
 	public static void cadastroAluno() {
 		
 		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Informe o nome do aluno(a): ");
+		String nome = leitor.nextLine();
+		
+		System.out.println("Informe o CPF do aluno(a): ");
+		int cpf = leitor.nextInt();
+		
+		System.out.println("Informe o RG do aluno(a): ");
+		int rg = leitor.nextInt();
+		
+		System.out.println("Informe a data de nascimento: ");
 		
 	}
 	
@@ -15,7 +27,7 @@ public class ModoNaoAluno {
 		
 	}
 		
-	}
+}
 	
 	
 
